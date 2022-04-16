102f07cc42f0a409a5bf0bf94fc0a1a56f7cdfc9
Ajay Gupta <ajaygpt28@gmail.com>
2022-04-16 16:38:01 +0530

    initial commit

---
+++ src/test/java/com/randomusers/userdatapack/UserdatapackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomusers.userdatapack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdatapackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
